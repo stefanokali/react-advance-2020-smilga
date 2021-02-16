9c922325aead501e95ad4cf61880cacf4fd17a3a
Stefano Kusuma Ali <stefano8795@gmail.com>
2021-02-16 22:44:31 +0800

    complete tutorial 6 to 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Setup from "./tutorial/8-useContext/setup/1-context-api";
 
 function App() {
   return (
