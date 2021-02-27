c5b3d3d29cbd2efa5f7fbe0f2dfc63997615073d
Stefano Kusuma Ali <stefano8795@gmail.com>
2021-02-27 21:38:20 +0800

    finish all tutorial

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const [name, setName] = useState("default");
+  const { id } = useParams();
+
+  useEffect(() => {
+    const newPerson = data.find((person) => {
+      return person.id === parseInt(id);
+    });
+    setName(newPerson.name);
+  }, []);
+
   return (
     <div>
-      <h2>person</h2>
+      <h1>{name}</h1>
+      <Link to="/people" className="btn">
+        Back To People
+      </Link>
     </div>
   );
 };
